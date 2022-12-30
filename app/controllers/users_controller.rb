074b6ba60348d4e24c588e70559d22314afd1c5d
EC2 Default User <ec2-user@ip-172-31-10-145.ec2.internal>
2022-12-30 05:51:14 +0000

    [保存]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,27 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
   end
 
   def index
     @users = User.all
+    @book = Book.new
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
- 
-  
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+    def user_params
+     params.require(:user).permit(:name,:introduction,:profile_image)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_one_attached :image
   has_many :books, dependent: :destroy
+  has_one_attached :profile_image
+  
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path),filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit:[width,height]).processed
+  end
 end
