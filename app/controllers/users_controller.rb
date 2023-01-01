e3fc3521a371ae38acbec4825cb7ce0b03d3b43a
nanaseni <ogawa.rei.infratop@gmail.com>
2023-01-01 15:06:25 +0000

    [update]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,11 +14,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+   
   end
 
   def update
-    @user = User.find(params[:id])
+    
     if @user.update(user_params)
       redirect_to user_path(@user.id),notice: "You have updated user successfully."
     else
@@ -28,12 +28,12 @@ class UsersController < ApplicationController
 
   private
     def user_params
-     params.require(:user).permit(:name,:introduction,:profile_image)
+     params.require(:user).permit(:name, :introduction, :profile_image)
     end
 
     def is_maching_login_user
-      user_id = params[:id].to_i
-      unless user_id == current_user.id
+      @user= User.find(params[:id])
+      unless @user.id == current_user.id
         redirect_to user_path(current_user.id)
       end
     end
