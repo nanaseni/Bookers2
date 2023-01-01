ae9b83b367370b8964b63614c42f7c6835233bd4
nanaseni <ogawa.rei.infratop@gmail.com>
2023-01-01 01:48:11 +0000

    [中断]3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,8 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :is_maching_login_user, only: [:edit,:update]
+  
+
   def new
     @book = Book.new
   end
@@ -6,8 +10,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to book_path(@book),notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -24,9 +32,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to books_path
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     redirect_to book_path(@book),notice: "You have update book successfully."
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -40,5 +51,14 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title,:body)
   end
+  
+  def is_maching_login_user
+    @book = Book.find(params[:id])
+    unless @book.user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+ 
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :is_maching_login_user, only: [:edit,:update]
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
@@ -16,12 +19,22 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      redirect_to user_path(@user.id),notice: "You have updated user successfully."
+    else
+      render :edit
+    end
   end
 
   private
     def user_params
      params.require(:user).permit(:name,:introduction,:profile_image)
+    end
+
+    def is_maching_login_user
+      user_id = params[:id].to_i
+      unless user_id == current_user.id
+        redirect_to user_path(current_user.id)
+      end
     end
 end
